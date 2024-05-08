e58ecd54d0123698c028e786d1fab590a941e72c
Phloh <168723187+Phl0h@users.noreply.github.com>
2024-05-08 13:15:59 +0000

    push it baby

--- src/03_prompt.js
+++ src/03_prompt.js
@@ -4,8 +4,3 @@ const prompt = require('prompt-sync')({sigint: true});
 let firstName = prompt("Vorname?: ");
 let familyName = prompt("Name?: ");
 console.log("Hallo, "+firstName,familyName+"!");
-
-// Test change
-// Test change 2
-// Test change 3
-// test change 4
