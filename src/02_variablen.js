7fcdbffffb5598f9dc6192cfc12c204a9e767dd2
Phloh <168723187+Phl0h@users.noreply.github.com>
2024-05-08 08:03:42 +0000

    variablen01

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -1,1 +1,17 @@
-console.log("hi");
+// Kommentar  |  Strg + #
+/**
+ * 
+ * Block-Kommentar
+ * 
+ * 
+ */
+
+/****** Variablen 01  **********/
+
+//console.log("Badaboom, Badabang");
+//console.log(Badaboom, Badabang);
+
+/* Deklaration + Wertzuweisung I */
+
+let firstName;
+console.log(firstName);
