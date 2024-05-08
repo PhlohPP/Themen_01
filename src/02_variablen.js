c28fd745561995924f6ef40a98ffe8f9698cbc8e
Phloh <168723187+Phl0h@users.noreply.github.com>
2024-05-08 09:54:46 +0000

    test für gsc

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -53,5 +53,8 @@ console.log("-------------------"); */
 let test; // Variable : Deklaration
 test = "hi"; // Wertzuweisung (Initialisierung)
 test = "hello"; // Überschreiben
-console.log("Inhalt " + test); // Ausgabe
+console.log("Inhalt: " + test); // Ausgabe
 
+// Konstante
+const test2 = "Hi!"; // Wertzuweisung muss sofort erfolgen
+console.log("Inhalt: " + test2); // Ausgabe
