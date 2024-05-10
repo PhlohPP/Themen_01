101ecdd2bac1a4e05bb3f8ad93972207f0a3fdac
Phloh <168723187+Phl0h@users.noreply.github.com>
2024-05-10 10:45:04 +0000

    tests if else

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -16,6 +16,7 @@ console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
 console.log("isJohnOlder: " + isJohnOlder);
 console.log("isJohnEqual: " + isJohnEqual);
+
 console.log("------------------");
 
 /************ IF  ************/
@@ -23,13 +24,43 @@ console.log("------------------");
 // entweder JA oder nix ... alternativlos
 // Türsteher-Politik (Doors 10 p.m.)
 
-// if (true) // statt Test das Testergebnis festgesetzt zu Prüfzwecken nützlich
-// {
-//     console.log("John ist älter!");
-// }
-
-if (isJohnOlder)
+// if (true) // statt Test das Testergebnis festgesetzt zu Prüfzwecken nützlich (fake it!)
+// if (isJohnOlder)  // Verweis auf ein Testergebnis
+if (ageJohn > ageMark) // Test vor Ort
     { // codeblock
         console.log("John ist älter!");
     }
-    //wenn der Test fehlschlägt, kommt nichts
+    //wenn der Test fehlschlägt, kommt nichts  
+
+console.log("------------------");
+
+/************ IF - ELSE ************/
+// mit Alternative
+// entweder ja oder nein
+
+if (isJohnOlder)
+{ // Ja-Zweig
+    console.log("John ist älter!");
+}
+else
+{ // Alternativ-Zweig
+    console.log("John ist jünger!");
+}   
+
+console.log("------------------");
+
+/************ IF - ELSE IF  ************/
+// mit alternativen Fällen (älter, jünger, gleich alt)
+
+if (isJohnOlder) // Test
+{ // ja-Zweig
+    console.log("John ist älter!");
+}
+else if (isJohnEqual)
+{ // Alternativzweig 1..n
+    console.log("John ist gleichalt!");   
+}
+else
+{ // gemeinsame Alternative
+    console.log("John ist jünger!");
+}
