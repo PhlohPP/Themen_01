9696845bcd464ac63c0de226dff92568c6527568
Phloh <168723187+Phl0h@users.noreply.github.com>
2024-05-13 09:07:04 +0000

    2b. Parametrisierung + Datenübergabe von AUSSEN

--- src/08_funktionen_l.js
+++ src/08_funktionen_l.js
@@ -2,10 +2,40 @@
 /******* Funktionen 01 *********/
 // 1. Kapselung von Codeblöcken
 
-function test(params)
+// Funktionsaufruf | call(er)
+// test();
+
+// Funktionsrumpf | callee
+// Funktionedeklaration
+function test()
 {
-    console.log("Allakhazam!")    
+    console.log("Alakazam!");    
 }
 
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
 
+// Funktionsaufruf
+// outputMagic(); 
 
+// Funktionedeklaration
+function outputMagic() 
+{
+    // interne Variable | What happens in VEGAS ...
+    let firstMagic = "Abra";
+    console.log("**~ " + firstMagic + "!" + " ~**");
+}
+
+// console.log(firstMagic) // Fehler : scope!
+
+/***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+outputMagic2("Abra"); // Argument --> Daten für Parameter
+outputMagic2("Cadabra");
+outputMagic2("Alakazam");
+
+function outputMagic2(magicWord) // Parameter
+{
+    console.log("**~ " + magicWord + "!" + " ~**");
+}
