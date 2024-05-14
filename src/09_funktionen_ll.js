e811308dbedaeaf21d9b4768bafb04da8f370738
Phloh <168723187+Phl0h@users.noreply.github.com>
2024-05-14 11:20:21 +0000

    end of day

--- src/09_funktionen_ll.js
+++ src/09_funktionen_ll.js
@@ -16,8 +16,7 @@ const INPUT_STR_NUM2 = "And another number, if you please =)  :  ";
 const INPUT_STR_OP = "Operator?  :  ";
 const RESULT_STR = "Aaaaaand a big round of applause for. Our! RESULT!!1  :  "
 const ERROR_STR_DIV = "Ooh no, you tried to divide by 0. Please, don't.";
-const ERROR_STR_GEN = "Something went wrong. Maybe check the cables and the hamster cage.";
-
+const ERROR_STR_GEN = "Something went wrong. Maybe, check the cables and the hamster cage.";
 
 startApp();
 function startApp() {
@@ -25,11 +24,11 @@ function startApp() {
 }
 
 function getNum1() {
-    return parseInt(prompt(INPUT_STR_NUM1));
+    return parseFloat(prompt(INPUT_STR_NUM1));
 }
 
 function getNum2() {
-    return parseInt(prompt(INPUT_STR_NUM2));
+    return parseFloat(prompt(INPUT_STR_NUM2));
 }
 
 function getOp() {
@@ -63,6 +62,30 @@ function calculator(a,b,op) {
 
 }
 
+// module: addition a + b |test:
+// output(add(2,2));
+// output(add(2,-3));
+// output(add(2,0));
+function add(a,b) {
+    return a + b;
+}
+
+// module: subtraction a - b |  test:
+// output(subtract(2,3));
+// output(subtract(2,-3));
+// output(subtract(2,0));
+function subtract(a,b) {
+    return a - b;
+}
+
+// module: multiplication a * b |  test:
+// output(multiply(2,3));
+// output(multiply(2,-3));
+// output(multiply(2,0));
+function multiply(a,b) {
+    return a * b;
+}
+
 // module: division a / b |  test:
 // output(divide(2,3));
 // output(divide(6,-3));
@@ -80,30 +103,6 @@ function divide(a,b) {
     else {
         return ERROR_STR_DIV;
     }
-}
-
-// module: multiplication a * b |  test:
-// output(multiply(2,3));
-// output(multiply(2,-3));
-// output(multiply(2,0));
-function multiply(a,b) {
-    return a * b;
-}
-
-// module: subtraction a - b |  test:
-// output(subtract(2,3));
-// output(subtract(2,-3));
-// output(subtract(2,0));
-function subtract(a,b) {
-    return a - b;
-}
-
-// module: addition a + b |test:
-// output(add(2,2));
-// output(add(2,-3));
-// output(add(2,0));
-function add(a,b) {
-    return a + b;
 }
 
 // module: output | test:
