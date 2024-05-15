67b7ec7cc08c60643e9e57c289feffbf95e7f7b1
Phloh_local <phloh1338@gmx.de>
2024-05-15 11:53:49 +0200

    arrays1

--- src/11_Arrays_I.js
+++ src/11_Arrays_I.js
@@ -20,17 +20,62 @@
 /* 02a. Theorie: Schleifen (for-schleife) */
 /* For -Schleifen als allg. Wiederholungs-Struktur */
 
+// Inkrement (untere Grenze --> obere Grenze)
 // for (let i = 0; i < 10; i++) // i = Schleifenindex
 // {
 //     output("index: " + i);
 // }
 
+// Dekrement (obere Grenze --> untere Grenze)
+// for (let i = 10; i > 0; i--)  // i = Schleifenindex
+// {
+//     output("index: " + i);
+// }
+
+// Inkrement (var. Schrittweite)
+// for (let i = 0; i <= 100; i += 5)  // i = Schleifenindex
+// {
+//     output("index: " + i);
+// }
+
 /* 02b. For-Schleife für Array-Index (Iteration)*/
-let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
-for (i = 0; i < arr.length; i++) {
-    output("inter: " + i);
-    output("extern: " + arr[i]); // Mapping
+// let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
+// for (i = 0; i < arr.length; i++) {
+//     output("inter: " + i);
+//     output("extern: " + arr[i]); // Mapping
+// }
+
+/********* Überlegungen - Transponierung **********/
+ 
+/*
+1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
+Solange die Variable existiert, bleibt dieser erhalten.
+hier: Aufsummierung
+*/
+
+/* Zu "Fuß" - DRY !!! */
+// let a = 0; // Anfangswert
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 0 + 1
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 1 + 1
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 2 + 1
+// output("inhalt von a: " + a);
+
+/* Besser: mit FOR-Schleife */
+ 
+let a = 0; // Anfangswert
+for (let i = 0; i < 10; i++) {
+    a = a + 1;
+    output("inhalt von a: " + a);
 }
+
+
+
+
+
+
 
 /**
  *   PRAXIS
@@ -74,6 +119,20 @@ function getSentenceArr(arr) {
     return str;
 }
 
+/*** 01b. Funktionalität mit Array 2  */
+// Transponierung:  untereinander ---> nebeneinander
+// Helge Schneider: Anananandereihung ...
+// output(getSentenceArr2(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr2(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+
+    for (let i = 0; i < arr.length; i++)  // i = Schleifenindex
+    {
+        output(arr[i]); 
+    }
+
+}
 
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
