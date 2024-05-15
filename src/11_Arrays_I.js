9bf3d40e969e37f7d10e5785aa046618d531ae1d
Phloh_local <phloh1338@gmx.de>
2024-05-15 10:44:30 +0200

    zwischenspeichern

--- src/11_Arrays_I.js
+++ src/11_Arrays_I.js
@@ -5,6 +5,20 @@
  *   THEORIE
  * 
  */
+// Theorie Arrays
+
+// let arr;
+// //arr = new Array(); // Konstruktor
+// // arr = [];
+// arr = [2,11,7,8,10]  // Array mit Elementen
+
+// output(arr); 
+// output(arr.length); // Anzahl der Elemente (.Eigenschaft)
+// output(arr[0]) // Index 0 (1. Pos. im Array)
+// output(arr[arr.length-1]) // letze Pos. // allgemein .. n-1
+
+/* 02a. Theorie: Schleifen (for-schleife) */
+/* For -Schleifen als allg. Wiederholungs-Struktur */
 
 
 
@@ -30,7 +44,25 @@ function getSentence(word1,word2,word3,word4) {
     return str;
 }
 
+/*** 01a. Funktionalität mit Array 1 */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
+output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   arr[0] + GAP +
+                arr[1] + GAP +
+                arr[2] + GAP +
+                arr[3] + GAP +
+                arr[4] + GAP + 
+                arr[5] + GAP +
+                PUNCT;
 
+    return str;
+}
 
 
 // Modul: Ausgabe in Konsole : Test
